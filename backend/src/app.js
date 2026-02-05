f7ce50450f722ea71076b023e1538ebecde86d80
ONKAR Raj <rajonkar96@gmail.com>
2026-02-05 21:32:53 +0530

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const authRouter = require("./routes/auth.routes");
 const levelRouter = require("./routes/level.routes");
 const submissionRouter = require("./routes/submission.routes");
 const xpRouter = require("./routes/xp.routes");
 const cookieParser = require("cookie-parser");
+
+// Enable CORS for all routes
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    credentials: true // Allow cookies
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
